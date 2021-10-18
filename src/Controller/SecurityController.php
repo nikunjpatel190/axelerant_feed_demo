30b32b62342a8326b26a8012085e38032f87022f
Nkunj Bambhroliya <nikunjpatel190@gmail.com>
2021-10-18 14:36:57 +0530

    Resolve redirect path

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -27,14 +27,14 @@ class SecurityController extends AbstractController
     {
         // if user is already logged in, don't display the login page again
         if ($security->isGranted('ROLE_USER')) {
-            return $this->redirectToRoute('blog_index');
+            return $this->redirectToRoute('home_index');
         }
 
         // this statement solves an edge-case: if you change the locale in the login
         // page, after a successful login you are redirected to a page in the previous
         // locale. This code regenerates the referrer URL whenever the login page is
         // browsed, to ensure that its locale is always the current one.
-        $this->saveTargetPath($request->getSession(), 'main', $this->generateUrl('home_index'));
+        $this->saveTargetPath($request->getSession(), 'main', $this->generateUrl('admin_feed_index'));
 
         return $this->render('security/login.html.twig', [
             // last username entered by the user (if any)
